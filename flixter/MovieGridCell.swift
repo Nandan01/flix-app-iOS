ad8b96c538764a2faa63c2ef4eb6f357cd68716e
Nandan01 <36855411+Nandan01@users.noreply.github.com>
2021-09-20 22:09:08 -0400

    flixapptwo

---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Sukhnandan Kaur on 9/20/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
